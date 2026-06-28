f945b20ece09d5d1ea397bfb5db52371af7c6ded
Priyanshu Singh <priyanshu30jsr@gmail.com>
2026-06-28 11:40:19 +0530

    fix: add Dockerfile and dockerignore

---
+++ cloud_env.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS
+SET PATH=C:\Users\LENOVO\Desktop\last-minute-life-saver\google-cloud-sdk\bin;%PATH%;
+cd C:\Users\LENOVO\Desktop\last-minute-life-saver
+ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
